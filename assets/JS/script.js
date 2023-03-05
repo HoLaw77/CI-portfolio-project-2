3c611b56888ec7b9ea48dfb2d03323155c93ae06
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-05 15:49:16 +0000

    update java

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,26 @@
-function 
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+
+});
+
