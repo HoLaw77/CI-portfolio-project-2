93ad90b3cf364ca7dd4dd983a7072e899f1ef5e9
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-05 17:29:55 +0000

    update java

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,6 +3,15 @@ function runGame(){}
 function checkAnswer(){}
 
 function calculateCorrectAnswer(){}
+document.addEventListener("DOMContentLoaded" function(){}){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener('click', function(){})
+        if(this.getAttribute("data-type") === "submit"){
+            alert("You clicked submit.")
+        }
+    }
+}
 
 function incrementScore(){}
 
