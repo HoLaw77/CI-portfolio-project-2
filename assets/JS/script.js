78e824c27f0971873348c8a14eb745e6267f625b
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-06 08:45:41 +0000

    update java

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,3 +3,44 @@ let playAction = document.getElementById("player-action");
 let gameResult = document.getElementById("result");
 let gameChoices = document.getElementsByTagName("button");
 
+document.addEventListener('Click', {function(){}} )
+
+function computerAction() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    
+    if (randomNumber === 1) {
+      computerAction = 'rock'
+    }
+    if (randomNumber === 2) {
+      computerAction = 'scissors'
+    }
+    if (randomNumber === 3) {
+      computerAction = 'paper'
+    }
+    computerActionDisplay.innerHTML = computerAction
+  }
+  
+  function gameResult() {
+    if (computerAction === userAction) {
+      result = 'its a draw!'
+    } else
+    if (computerAction === 'rock' && userAction === "paper") {
+      result = 'you win!'
+    } else
+    if (computerAction === 'rock' && userAction === "scissors") {
+      result = 'you lost!'
+    } else
+    if (computerActoin === 'paper' && userAction === "scissors") {
+      result = 'you win!'
+    } else
+    if (computerAction === 'paper' && userAction === "rock") {
+      result = 'you lose!'
+    } else
+    if (computerAction === 'scissors' && userAction === "rock") {
+      result = 'you win!'
+    } else
+    if (computerAction === 'scissors' && userAction === "paper") {
+      result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+  }
