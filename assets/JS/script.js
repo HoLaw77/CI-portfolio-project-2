2e91871a6436ede2337747761cbb017c3a81dc47
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-06 20:45:11 +0000

    debug if statment

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,25 +7,24 @@ document.getElementById("scissors").addEventListener('click', scissorsChoice);
 function scissorsChoice(){ playerChoice = "scissors"; alert ("You click scissors.");}
 document.getElementById("restart").addEventListener('click', clearGame);
 function clearGame(){ alert ("You click restart.");}
-let computerChoice = document.getElementById("computer-action");
-let gameResult = document.getElementById("result");
+
 
 /*function for math random*/
 function generateMath(){let randomNumber = Math.floor(Math.random()* 3);
   if (randomNumber === 1){computerChoice = "paper"};
   if (randomNumber === 2){computerChoice = "scissors"};
   if (randomNumber === 3){computerChoice = "rock"};
-  
+  computerChoice = computerChoiceDisplay.innerHTML;
 }
 
 function gameResult(){
   if (playerChoice == computerChoice){ gameResult = "draw"} else
-  if (playerChoice == "paper" || computerChoice == "scissors"){gameResult = "You lose."} else
-  if (playerChoice == "paper" || computerChoice == "rock"){gameResult = "You Win."} else
-  if (playerChoice == "rock"  || computerChoice == "scissors"){gameResult = "You win."} else
-  
+  if (playerChoice == "paper" && computerChoice == "scissors"){gameResult = "You lose."} else
+  if (playerChoice == "paper" && computerChoice == "rock"){gameResult = "You Win."} else
+  if (playerChoice == "rock"  && computerChoice == "scissors"){gameResult = "You win."} else 
+  gameResult = gameResultDisplay.innerHTML; 
 }
 
 function printGameResult(){
-  if(gameResult == "draw"){}
+
 }
