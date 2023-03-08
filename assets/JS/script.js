7ae698f67bfbf6f0fcff0398cc4b8800890b20d7
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-08 21:55:22 +0000

    change the java to old version

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,29 +1,15 @@
 /*set variables for storing values*/
-const playerChoice = document.getElementById("player-action");
-const computerChoice = document.getElementById("computer-action");
-const gameResultDisplay = document.getElementById("result");
-const gameButtons = document.getElementsByTagName("button");
-let player;
-let computer;
-let result;
 
 
-gameButtons.forEach (buttons => buttons.addEventListener('click', () => {
-    player = buttons.textContent;
-    generateMath();
-    playerChoice.textContent = `Your Choice: ${player}`;
-    computerChoice.textContent = `Computer action: ${computer}`;
-    gameResultDisplay.textContent = gameResult();
-  }));
 /*event buttons testing using alert*/
-/*document.getElementById("paper").addEventListener('click', paperChoice);
+document.getElementById("paper").addEventListener('click', paperChoice);
 function paperChoice(){ playerChoice = "paper"; alert ("You click paper.");}
 document.getElementById("rock").addEventListener('click', rockChoice);
 function rockChoice(){ playerChoice = "rock"; alert ("You click rock.");}
 document.getElementById("scissors").addEventListener('click', scissorsChoice);
 function scissorsChoice(){ playerChoice = "scissors"; alert ("You click scissors.");}
 document.getElementById("restart").addEventListener('click', clearGame);
-function clearGame(){ alert ("You click restart.");}*/
+function clearGame(){ alert ("You click restart.");}
 
 
 
